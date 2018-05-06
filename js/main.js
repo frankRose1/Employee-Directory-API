e8d2b7b2ab7c4247763bd2bd6b1e3e2e55fe635d
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-05-05 23:45:46 -0400

    added code to get employees name, email, and location and update the html

--- js/main.js
+++ js/main.js
@@ -3,8 +3,7 @@ const userInfo = document.querySelector('div.user-info');
 // FETCH API
 //--------------
 fetchData('https://randomuser.me/api/')
-    .then(data => getImage(data.results[0].picture.large))
-
+    .then(data => getEmployeeInfo(data.results[0]))
 
 //--------------
 // FUNCTIONS
@@ -16,8 +15,21 @@ return fetch(url)
         .catch(error => console.log(`It looks like we encountered an error!, ${error}`))
 }
 
-function getImage(data) {
+function getEmployeeInfo(data) {
     const gridBox = document.querySelectorAll('.grid-box')[0];
+    //img info
     const img = document.getElementById('api-img');
-    img.src = data;
+    img.src = data.picture.large;
+    
+    //name info
+    const empName = document.querySelector('h2.name');
+    empName.innerHTML = `${data.name.first} ${data.name.last}`;
+    
+    //email info
+    const email = document.querySelector('p.email');
+    email.innerHTML = data.email;
+    
+    //location info
+    const location = document.querySelector('p.location');
+    location.innerHTML = data.location.city;
 }
