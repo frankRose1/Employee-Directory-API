11e91c0639f6852c9fa279831f35359b5ab181fb
Frank Rosendorf <frank.rosendorf1@gmail.com>
2018-05-06 17:22:32 -0400

    finished the markup and styling for the overlay and modal

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
+const body = document.querySelector('body');
+const gridContainer = document.querySelector('.grid');
 const grid = document.querySelectorAll('.grid-box');
+const overlay = document.querySelector('.overlay');
+const closeIcon = document.querySelector('.close');
+
 //--------------
 // FETCH API
 //--------------
-for (let i = 0; i < grid.length; i++) {
-    fetchData('https://randomuser.me/api/')
-    .then(data => getEmployeeInfo(data.results[0], i))
-}
+grid.forEach( gridBox => {
+     fetchData('https://randomuser.me/api/')
+    .then(data => getEmployeeInfo(data.results[0], gridBox))
+});
 
 //--------------
 // FUNCTIONS
@@ -17,22 +22,37 @@ return fetch(url)
         .catch(error => console.log(`It looks like we encountered an error!, ${error}`))
 }
 
-function getEmployeeInfo(data, i) {
-    let gridBox = grid[i];
+function getEmployeeInfo(data, gridBox) {
+    let employeeGrid = gridBox;
 
     //img info
-    const img = gridBox.querySelector('img.api-img');
+    const img = employeeGrid.querySelector('img.api-img');
     img.src = data.picture.large;
     
     //name info
-    const empName = gridBox.querySelector('h2.name');
+    const empName = employeeGrid.querySelector('h2.name');
     empName.innerHTML = `${data.name.first} ${data.name.last}`;
     
     //email info
-    const email = gridBox.querySelector('p.email');
+    const email = employeeGrid.querySelector('p.email');
     email.innerHTML = data.email;
     
     //location info
-    const location = gridBox.querySelector('p.location');
+    const location = employeeGrid.querySelector('p.location');
     location.innerHTML = data.location.city;
 }
+
+
+//--------------
+// EVENT HANDLERS
+//--------------
+
+gridContainer.addEventListener('click', () => {
+    overlay.style.display = 'block';
+}); 
+
+body.addEventListener('click', (e) => {
+    if (e.target == overlay || e.target == closeIcon) {
+        overlay.style.display = 'none';
+    }
+});
